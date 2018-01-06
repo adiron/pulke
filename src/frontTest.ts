0fefcab3a79951923982d9fbc03b84b1a95de4c4
Adi Ron <adi@adiron.me>
2018-01-06 12:17:26 +0200

    Changed the frontend test

--- src/frontTest.ts
+++ src/frontTest.ts
@@ -6,14 +6,24 @@ let pulke = new Pulke({
     loop: true,
     loopTimes: 0,
     items: [{selector: ".ball", props: [{
+        property: "height",
+        keyframes: [
+            {position: 0, value: 120},
+            {position: 0.5, value: 50},
+            {position: 0.6, value: 30},
+            {position: 1, value: 120}
+        ]
+    },
+    {
         property: "width",
         keyframes: [
-            {position: 0, value: 20},
+            {position: 0, value: 120},
             {position: 0.5, value: 50},
-            {position: 0.6, value: 10},
-            {position: 1, value: 70}
-        ]
-    }]}]
+            {position: 1, value: 120}
+        ],
+        unit: "%"
+    }
+  ]}]
 })
 
 pulke.play();
