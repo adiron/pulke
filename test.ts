1ff1844ae7796840006e763044a949f8bf393d55
Adi Ron <adi@adiron.me>
2018-04-07 12:27:46 +0300

    Added utils to the test suite

--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@ import 'mocha';
 import { Pulke } from './src/Pulke';
 import { AnimationController, AnimPropController } from './src/Controllers';
 import { detectEase } from './src/Ease';
+import utils from './src/utils';
 
 describe('Animation specs calculations', () => {
 
