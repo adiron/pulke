2b711fb853a7264dc6198e2a53db3a9f2da24e76
Adi Ron <adi@adiron.me>
2018-04-08 21:17:42 +0300

    Removed dist/

--- dist/src/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function mapRange(value: number, low1: number, high1: number, low2: number, high2: number): number;
-export declare function clamp(value: number, min: number, max: number): number;
